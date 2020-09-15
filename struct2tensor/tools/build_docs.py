320307fdda8f84e8502b960f2a7f08472755e2c6
andylou <andylou@google.com>
2020-09-15 11:01:29 -0700

    update doc generation to do expression_impl and s2t's root seperately.
    PiperOrigin-RevId: 331807910

--- struct2tensor/tools/build_docs.py
+++ struct2tensor/tools/build_docs.py
@@ -19,15 +19,18 @@ blaze run //struct2tensor/opensource_only/tools:build_docs -- \
 
 
 import inspect
-
+import pathlib
+import shutil
+import tempfile
 
 from absl import app
 from absl import flags
-
 import struct2tensor as s2t
-
+from struct2tensor import expression_impl
 from tensorflow_docs.api_generator import generate_lib
 from tensorflow_docs.api_generator import public_api
+
+import yaml
 
 flags.DEFINE_string("output_dir", "/tmp/s2t_api", "Where to output the docs")
 
@@ -35,7 +38,6 @@ flags.DEFINE_string(
     "code_url_prefix",
     "https://github.com/google/struct2tensor/blob/master/struct2tensor",
     "The url prefix for links to code.")
-
 
 flags.DEFINE_bool("search_hints", True,
                   "Include metadata search hints in the generated files")
@@ -78,22 +80,63 @@ def _filter_module_attributes(path, parent, children):
 
 def main(unused_argv):
 
+  # TODO(andylou): write simple unit test
+  s2t_out = pathlib.Path(tempfile.mkdtemp())
   doc_generator = generate_lib.DocGenerator(
       root_title="Struct2Tensor",
       py_modules=[("s2t", s2t)],
       code_url_prefix=FLAGS.code_url_prefix,
       search_hints=FLAGS.search_hints,
-      # Use private_map to exclude doc locations by name if excluding by object
-      # is insufficient.
-      private_map={},
-      # local_definitions_filter ensures that shared modules are only
-      # documented in the location that defines them, instead of every location
-      # that imports them.
+      # explicit_package_contents_filter ensures that only modules imported
+      # directly from s2t/__init__.py are documented in the location that
+      # defines them, instead of every location that imports them.
       callbacks=[
-          public_api.local_definitions_filter, _filter_module_attributes
+          public_api.explicit_package_contents_filter, _filter_module_attributes
       ])
 
-  return doc_generator.build(output_dir=FLAGS.output_dir)
+  doc_generator.build(s2t_out)
+
+  expr_impl_out = pathlib.Path(tempfile.mkdtemp())
+  doc_generator = generate_lib.DocGenerator(
+      root_title="Struct2Tensor-expression_impl",
+      py_modules=[("expression_impl", expression_impl)],
+      code_url_prefix=FLAGS.code_url_prefix + "/expression_impl",
+      search_hints=FLAGS.search_hints,
+      # explicit_package_contents_filter ensures that only modules imported
+      # directly from s2t/expression_impl/__init__.py are documented in the
+      # location that defines them, instead of every location that imports them.
+      callbacks=[
+          public_api.explicit_package_contents_filter, _filter_module_attributes
+      ])
+  doc_generator.build(expr_impl_out)
+
+  output_dir = pathlib.Path(FLAGS.output_dir)
+
+  def splice(name, tmp_dir):
+    shutil.rmtree(output_dir / name, ignore_errors=True)
+    shutil.copytree(tmp_dir / name, output_dir / name)
+    shutil.copy(tmp_dir / f"{name}.md", output_dir / f"{name}.md")
+    try:
+      shutil.copy(tmp_dir / "_redirects.yaml",
+                  output_dir / name / "_redirects.yaml")
+    except FileNotFoundError:
+      pass
+    shutil.copy(tmp_dir / "_toc.yaml", output_dir / name / "_toc.yaml")
+
+  splice("s2t", s2t_out)
+  splice("expression_impl", expr_impl_out)
+
+  toc_path = output_dir / "_toc.yaml"
+  toc_text = yaml.dump({
+      "toc": [{
+          "include": "/api_docs/python/s2t/_toc.yaml"
+      }, {
+          "break": True
+      }, {
+          "include": "/api_docs/python/expression_impl/_toc.yaml"
+      }]
+  })
+  toc_path.write_text(toc_text)
 
 
 if __name__ == "__main__":
