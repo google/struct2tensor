d58da3014aeedb6d3de9e645bce2fae63c089b46
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-08-14 20:15:14 -0700

    Make compliant with Ruff rule D200

--- struct2tensor/benchmarks/struct2tensor_benchmark.py
+++ struct2tensor/benchmarks/struct2tensor_benchmark.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-r"""Benchmarks for struct2tensor.
-
-
-"""
+r"""Benchmarks for struct2tensor."""
 
 import tensorflow as tf
 from absl.testing import parameterized
