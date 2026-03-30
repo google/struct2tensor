a48e9635d14cca9a7738e6962b78c11c4d90edf0
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-03-30 08:26:59 +0000

    Use repo_env in configure.sh

--- configure.sh
+++ configure.sh
@@ -24,9 +24,10 @@ function write_to_bazelrc() {
   echo "$1" >> .bazelrc
 }
 
-function write_action_env_to_bazelrc() {
-  write_to_bazelrc "build --action_env $1=\"$2\""
+function write_repo_env_to_bazelrc() {
+  write_to_bazelrc "build --repo_env $1=\"$2\""
 }
+
 
 function has_tensorflow() {
   ${PYTHON_BIN_PATH} -c "import tensorflow" > /dev/null
@@ -64,7 +65,7 @@ ensure_tensorflow
 TF_CFLAGS=( $(${PYTHON_BIN_PATH} -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS="$(${PYTHON_BIN_PATH} -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')"
 
-write_action_env_to_bazelrc "TF_HEADER_DIR" ${TF_CFLAGS:2}
+write_repo_env_to_bazelrc "TF_HEADER_DIR" ${TF_CFLAGS:2}
 SHARED_LIBRARY_DIR=${TF_LFLAGS:2}
 SHARED_LIBRARY_NAME=$(echo $TF_LFLAGS | rev | cut -d":" -f1 | rev)
 if ! [[ $TF_LFLAGS =~ .*:.* ]]; then
@@ -74,5 +75,5 @@ if ! [[ $TF_LFLAGS =~ .*:.* ]]; then
     SHARED_LIBRARY_NAME="libtensorflow_framework.so"
   fi
 fi
-write_action_env_to_bazelrc "TF_SHARED_LIBRARY_DIR" ${SHARED_LIBRARY_DIR}
-write_action_env_to_bazelrc "TF_SHARED_LIBRARY_NAME" ${SHARED_LIBRARY_NAME}
+write_repo_env_to_bazelrc "TF_SHARED_LIBRARY_DIR" ${SHARED_LIBRARY_DIR}
+write_repo_env_to_bazelrc "TF_SHARED_LIBRARY_NAME" ${SHARED_LIBRARY_NAME}
