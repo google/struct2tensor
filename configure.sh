592f758ad89fd384be78daef6e726e4ca2559e9a
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2026-04-08 12:15:17 -0700

    Update configure.sh
    Removed extra spaces

--- configure.sh
+++ configure.sh
@@ -28,8 +28,6 @@ function write_action_env_to_bazelrc() {
   write_to_bazelrc "build --action_env $1=\"$2\""
 }
 
-
-
 function has_tensorflow() {
   ${PYTHON_BIN_PATH} -c "import tensorflow" > /dev/null
 }
